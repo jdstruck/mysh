3a144796a154eca984ec099fcfa9f30c0664ee72
Jesse Struck <jdstruck@gmail.com>
2020-01-29 10:16:52 -0500

    add fork example

--- mysh.c
+++ mysh.c
@@ -1,7 +1,37 @@
 #include "mysh.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(){
-	printf("hello");	
+	pid_t pid;
+	int status;
+	int ret;
+
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork failed:");
+		exit(1);
+	}
+
+	if (pid == 0) {
+		printf("This is the child\n");
+		exit(99);
+	}
+
+	if (pid > 0) {
+		printf("This is parent. The child is %d\n", pid);
+
+		ret = waitpid(pid, &status, 0);
+		if (ret < 0) {
+			perror("waitpid failed:");
+			exit(2);
+		}
+
+		printf("Child exigted with status %d\n", WEXITSTATUS(status));
+	}
+	return 0;
 }
