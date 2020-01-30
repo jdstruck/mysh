94f30008ddba32e4035ad5d40c5b0315199560ec
Jesse Struck <jdstruck@gmail.com>
2020-01-30 16:36:27 -0500

    begin again

--- mysh.c
+++ mysh.c
@@ -1,55 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include "mysh.h"
 
 int main(void) {
-  // print prompt (>) to terminal
-  // read command from terminal
-  // pid=fork()
-  // if(pid==0) {
-  //	rc=exec(command)
-  //	if(rc!=0) {print error; continue;}
-  //}
-  // cmdrc=waitpid(pid)
+  FILE *stream;
+  char *lineptr = NULL;
+  size_t len = MAX_LINE_LEN;
 
-  // duplicate pathname in exec, i.e. execlp(pathname, arg0, .. argn) is there
-  // for aliasing
+  assert((stream = fdopen(STDIN_FILENO, "r")) != NULL);
+  while (1) {
+    printf("jsh> ");
+    getline(&lineptr, &len, stream);
+    printf("Shell read this line: %s\n", lineptr);
 
-  // READ ABOUT Virtual Memory PAGING
-  // Why 4K pages??
-
-  pid_t child_pid;
-
-  // Create child process
-  child_pid = fork();
-
-  // Check if it was successful to create child process
-  if (child_pid == -1) {
-    perror("fork unsuccessful");
-    exit(EXIT_FAILURE);
-  }
-
-  // Parent is waiting for the child to complete its task
-  if (child_pid == 0) {
-    printf("### Child ###\nCurrent PID: %d and Child PID: %d\n", getpid(),
-           child_pid);
-    int exec_return_value = execlp("ls", "ls", (char *)NULL);
-    if (exec_return_value == -1) {
-      perror("execlp failed");
-      exit(EXIT_FAILURE);
+    if (*lineptr == 'x') {
+      printf("exiting\n");
+      exit(0);
     }
-  } else {
-    printf("### Parent ###\nCurrent PID: %d and Child PID: %d\n", getpid(),
-           child_pid);
-    int wait_status;
-    pid_t terminated_child_pid = wait(&wait_status);
-    if (terminated_child_pid == -1) {
-      perror("wait failure");
-      exit(EXIT_FAILURE);
-    } else
-      printf("Parent: my child %d terminates.\n", terminated_child_pid);
   }
-
-  exit(EXIT_SUCCESS);
+  return 0;
 }
--- mysh.h
+++ mysh.h
@@ -1,6 +1,19 @@
 #ifndef _MYSH_H_
 #define _MYSH_H_
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+#define MAX_TOKENS 100
+#define MAX_STRING_LEN 1024
+
+size_t const MAX_LINE_LEN = 10000;
+
+#define EXIT_STR "exit"
+#define EXIT_CMD 0
+#define UNKNOWN_CMD 99
 
 #endif
