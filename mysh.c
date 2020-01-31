98f2b14e6ce919733d250ee7ba2857610b01d33c
Jesse Struck <jdstruck@gmail.com>
2020-01-31 08:35:06 -0500

    integrate tokenize(), run_command(), and read_command()

--- mysh.c
+++ mysh.c
@@ -7,26 +7,28 @@ char **tokens;
 
 int main(void) {
   assert((stream = fdopen(STDIN_FILENO, "r")) != NULL);
-  while (1) {
+  do {
     printf("mysh> ");
     read_command();
-  }
+  } while (run_command() != 0);
   return 0;
 }
 
 int run_command() {
 
-  if (strcmp(lineptr, "x") || strcmp(lineptr, "exit")) {
+  printf("lineptr: %s\n", lineptr);
+  printf("strcmp(lineptr, \"exit\") = %d\n", strcmp(lineptr, "exit"));
+  if (strcmp(lineptr, "exit")) {
     printf("exiting\n");
-    exit(EXIT_SUCCESS);
+    return 1;
   }
-  return 0;
+
+  return 1;
 }
 
 void read_command() {
   getline(&lineptr, &len, stream);
   tokenize(lineptr);
-  run_command();
 }
 
 void tokenize(char *string) { printf("Shell read this line: %s\n", lineptr); }
