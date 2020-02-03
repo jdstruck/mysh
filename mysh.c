45b04eddd4b9218cb421278d27f774c289da19ec
Jesse Struck <jdstruck@gmail.com>
2020-02-03 10:10:18 -0500

    execvp in run_command() works now

--- mysh.c
+++ mysh.c
@@ -14,21 +14,9 @@ int main(void) {
   return 0;
 }
 
-int run_command() {
-
-  printf("lineptr: %s\n", lineptr);
-  printf("strcmp(tokens[0], \"exit\") = %d\n", strcmp(tokens[0], "exit"));
-
-  if (strcmp(tokens[0], "exit") == 0 || strcmp(tokens[0], "x") == 0) {
-    return EXIT_CMD;
-  }
-
-  return UNKNOWN_CMD;
-}
-
 void read_command() {
   getline(&lineptr, &MAX_LINE_LEN, stream);
-  printf("Shell read this line: %s\n", lineptr);
+  // printf("Shell read this line: %s\n", lineptr);
   tokenize(lineptr);
 }
 
@@ -42,10 +30,45 @@ void tokenize(char *string) {
       continue;
 
     tokens[token_count] = this_token;
-    printf("Token %d: %s\n", token_count, tokens[token_count]);
+    // printf("Token %d: %s\n", token_count, tokens[token_count]);
     token_count++;
   }
-  print_tokens();
+  // print_tokens();
+}
+
+int run_command() {
+
+  // printf("strcmp(tokens[0], \"exit\") = %d\n", strcmp(tokens[0], "exit"));
+
+  if (strcmp(tokens[0], "exit") == 0 || strcmp(tokens[0], "x") == 0) {
+    return EXIT_CMD;
+  }
+
+  pid_t child_pid;
+  child_pid = fork();
+  if (child_pid == -1) {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+
+  if (child_pid == 0) {
+    int exec_return_value = execvp(tokens[0], tokens);
+    if (exec_return_value == -1) {
+      perror("execvp");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    int wait_status;
+    pid_t terminated_child_pid = wait(&wait_status);
+    if (terminated_child_pid == -1) {
+      perror("wait");
+      exit(EXIT_FAILURE);
+    } else {
+      printf("\nParent: my child %d terminates.\n", terminated_child_pid);
+    }
+  }
+
+  return UNKNOWN_CMD;
 }
 
 void print_tokens() {
