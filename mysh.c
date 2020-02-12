417a088a5e98a13ba921f421880ded21d680ecc7
Jesse Struck <jdstruck@gmail.com>
2020-02-12 09:46:42 -0500

    update

--- mysh.c
+++ mysh.c
@@ -1,13 +1,15 @@
 #include "mysh.h"
 
-int main(void) {
+int main(void)
+{
 
   lineptr = malloc(sizeof(char) * MAX_STRING_LEN);
   stream = fdopen(STDIN_FILENO, "r");
   tokens = malloc(sizeof(char *) * MAX_TOKENS);
   pipe_tokens = malloc(sizeof(char *) * MAX_TOKENS);
 
-  do {
+  do
+  {
     printf("mysh> ");
     read_command();
   } while (run_command() != EXIT_CMD);
@@ -15,18 +17,23 @@ int main(void) {
   return 0;
 }
 
-void read_command() {
+void read_command()
+{
   getline(&lineptr, &MAX_LINE_LEN, stream);
   // printf("Shell read this line: %s\n", lineptr);
   tokenize(lineptr);
 }
 
-void pipe_tokenize(char *pipe_str_p) {
+void pipe_tokenize(char *pipe_str_p)
+{
   int pipe_token_count = 0;
   printf("======PIPES======\n");
-  while (*pipe_str_p != '\0') {
+  while (*pipe_str_p != '\0')
+  {
+
     printf("%c", *pipe_str_p);
-    if (*pipe_str_p == '|') {
+    if (*pipe_str_p == '|')
+    {
       printf("\n");
     }
     pipe_str_p++;
@@ -34,13 +41,15 @@ void pipe_tokenize(char *pipe_str_p) {
   }
   printf("====END PIPES====\n");
 }
-void tokenize(char *string) {
+void tokenize(char *string)
+{
   int token_count = 0;
   // int size = MAX_TOKENS;
   char *this_token;
   pipe_tokenize(string);
 
-  while ((this_token = strsep(&string, " \t\v\f\n\r")) != NULL) {
+  while ((this_token = strsep(&string, " \t\v\f\n\r")) != NULL)
+  {
     printf("this_token = %s\n", this_token);
     if (*this_token == '\0')
       continue;
@@ -52,34 +61,44 @@ void tokenize(char *string) {
   print_tokens();
 }
 
-int run_command() {
+int run_command()
+{
 
   // printf("strcmp(tokens[0], \"exit\") = %d\n", strcmp(tokens[0], "exit"));
 
-  if (strcmp(tokens[0], "exit") == 0 || strcmp(tokens[0], "x") == 0) {
+  if (strcmp(tokens[0], "exit") == 0 || strcmp(tokens[0], "x") == 0)
+  {
     return EXIT_CMD;
   }
 
   pid_t child_pid;
   child_pid = fork();
-  if (child_pid == -1) {
+  if (child_pid == -1)
+  {
     perror("fork");
     exit(EXIT_FAILURE);
   }
 
-  if (child_pid == 0) {
+  if (child_pid == 0)
+  {
     int exec_return_value = execvp(tokens[0], tokens);
-    if (exec_return_value == -1) {
+    if (exec_return_value == -1)
+    {
       perror("execvp");
       exit(EXIT_FAILURE);
     }
-  } else {
+  }
+  else
+  {
     int wait_status;
     pid_t terminated_child_pid = wait(&wait_status);
-    if (terminated_child_pid == -1) {
+    if (terminated_child_pid == -1)
+    {
       perror("wait");
       exit(EXIT_FAILURE);
-    } else {
+    }
+    else
+    {
       printf("\nParent: my child %d terminates.\n", terminated_child_pid);
     }
   }
@@ -87,36 +106,42 @@ int run_command() {
   return UNKNOWN_CMD;
 }
 
-void print_tokens() {
+void print_tokens()
+{
   for (int i = 0; i < sizeof(tokens); ++i)
     printf("%s ", tokens[i]);
 
   printf("\n");
 }
-void pipe_recursive() {
+void pipe_recursive()
+{
   int fds[2];
   char buf[30];
   pid_t pid1, pid2, pid;
   int status, i;
 
   /* create a pipe */
-  if (pipe(fds) == -1) {
+  if (pipe(fds) == -1)
+  {
     perror("pipe");
     exit(1);
   }
 
   /* fork first child */
-  if ((pid1 = fork()) < 0) {
+  if ((pid1 = fork()) < 0)
+  {
     perror("fork");
     exit(1);
   }
 
-  if (pid1 == 0) {
+  if (pid1 == 0)
+  {
     close(1);        /* close normal stdout (fd = 1) */
     dup2(fds[1], 1); /* make stdout same as fds[1] */
     close(fds[0]);   /* we don't need the read end -- fds[0] */
 
-    if (execlp("ps", "ps", "-elf", (char *)0) < 0) {
+    if (execlp("ps", "ps", "-elf", (char *)0) < 0)
+    {
       perror("Child");
       exit(0);
     }
@@ -125,17 +150,20 @@ void pipe_recursive() {
   }
 
   /* fork second child */
-  if ((pid2 = fork()) < 0) {
+  if ((pid2 = fork()) < 0)
+  {
     perror("fork");
     exit(1);
   }
 
-  if (pid2 == 0) {
+  if (pid2 == 0)
+  {
     close(0);        /* close normal stdin (fd = 0)*/
     dup2(fds[0], 0); /* make stdin same as fds[0] */
     close(fds[1]);   /* we don't need the write end -- fds[1]*/
 
-    if (execlp("less", "less", (char *)0) < 0) {
+    if (execlp("less", "less", (char *)0) < 0)
+    {
       perror("Child");
       exit(0);
     }
@@ -148,7 +176,8 @@ void pipe_recursive() {
   close(fds[1]);
 
   /* parent waits for children to complete */
-  for (i = 0; i < 2; i++) {
+  for (i = 0; i < 2; i++)
+  {
     pid = wait(&status);
     printf("Parent: Child %d completed with status %d\n", pid, status);
   }
